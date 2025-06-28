0c39fa4927d3085c9acebb2e8de743eb95fef80a
Jimi Jokela <jimi.jokela@live.fi>
2025-06-28 12:54:40 +0300

    Generate tiles

--- NamazuCityBuilder/Namazu/Assets/Scripts/GameManager.cs
+++ NamazuCityBuilder/Namazu/Assets/Scripts/GameManager.cs
@@ -8,6 +8,30 @@ public class GameManager : MonoBehaviour
     public BuildingType SelectedBuildingType;
     public Transform BuildingsParent;
     public List<BuildingType> BuildingTypes;
+    public List<GameObject> TilePrefabs;
+    public Transform TilesParent;
+    public Tile[,] Tiles;
+    public int TileCountY;
+    public int TileCountX;
+
+    void Start()
+    {
+        GenerateTiles();
+    }
+
+    public void GenerateTiles()
+    {
+        Tiles = new Tile[TileCountX, TileCountY];
+        for (int y = 0; y < TileCountY; y++)
+        {
+            for (int x = 0; x < TileCountX; x++)
+            {
+                GameObject tileGo = Instantiate(TilePrefabs[Random.Range(0, TilePrefabs.Count)], TilesParent);
+                tileGo.transform.localPosition = new Vector3(x * 32, y * 32, 0);
+                Tiles[x, y] = tileGo.GetComponent<Tile>();
+            }
+        }
+    }
 
     public void SelectBuildingType1()
     {
